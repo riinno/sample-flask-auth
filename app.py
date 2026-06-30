09524e94b4dd9af701bcddefe4fcb73cdd011049
riinno <bryno.bdx1@gmail.com>
2026-06-29 22:13:47 -0300

    DB: Configurating db

--- app.py
+++ app.py
@@ -1,25 +1,34 @@
 # by Riinno, 2026
 
-# -------------------------------------------
+# --------------------------------------------------------
 #Importação de dependencias e variaveis base
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+
+# Criando uma chave secreta pro db
+# Para estudo, está sendo definida com uma string
+app.config['SECRET_KEY'] = "your_secret_key"
+
+# Definindo o caminho para o SQLAlchemy se conetar ao db
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+
 db = SQLAlchemy(app)
 
-# -------------------------------------------
+# --------------------------------------------------------
 # Rota Hello World (para testes iniciais)
 
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
   return "hello world!"
 
-# -------------------------------------------
+# --------------------------------------------------------
 # Rota para inicialização manual
 
 if __name__ == "__main__":
   app.run(debug=True)
 
-# -------------------------------------------
+# --------------------------------------------------------
