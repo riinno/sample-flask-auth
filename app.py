8abdaf4dd2e39854c8d3ea16645b47ff3f1a7db7
riinno <bryno.bdx1@gmail.com>
2026-06-30 14:16:05 -0300

    ROUTE: Creating base 'Login' route - without real functionality, just simple test for invalid infos

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 # by Riinno, 2026
 
-# -------------------------------------------------------------
+# --------------------------------------------------------------------------
 #Importação de dependencias e variaveis base
 
-from flask import Flask
+from flask import Flask, request, jsonify
 from models.user import User
 from database import db
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -16,20 +17,42 @@ app.config['SECRET_KEY'] = "your_secret_key"
 # Definindo o caminho para o SQLAlchemy se conetar ao db
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 
-# Inicializando o app de db (de database.py) com o 'app.py'
-db.init_app(app)
+login_manager = LoginManager()
 
-# -------------------------------------------------------------
+# Inicializando o app de db e login_manager (de database.py) com o 'app.py'
+db.init_app(app)
+login_manager.init_app(app)
+# criar mais tarde o view login
+
+# --------------------------------------------------------------------------
+# Rota Login
+
+@app.route("/login", methods=["POST"])
+def login():
+  data = request.get_json()
+  username = data["username"]
+  password = data["password"]
+
+  # se alguma das infos for vazia, volta erro
+  if not username or not password:
+    return jsonify({"message": "Credenciais inválidas"}), 400
+  
+  # do contrario, deu certo
+
+  # Login
+  return jsonify({"message": "Autenticação realizada com sucesso"})
+
+# --------------------------------------------------------------------------
 # Rota Hello World (para testes iniciais)
 
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
   return "hello world!"
 
-# -------------------------------------------------------------
+# --------------------------------------------------------------------------
 # Rota para inicialização manual
 
 if __name__ == "__main__":
   app.run(debug=True)
 
-# -------------------------------------------------------------
+# --------------------------------------------------------------------------
